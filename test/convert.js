8151afb2e26eceb01e71847c049a429112bf953b
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-05-19 18:35:13 +0200

    Fix input piping

--- test/convert.js
+++ test/convert.js
@@ -1,3 +1,4 @@
+const {join} = require('path')
 const test = require('ava')
 const devnull = require('dev-null')
 const {convert} = require('..')
@@ -12,5 +13,10 @@ test('convert: regular FeatureCollection', async t => {
 
 test('convert: empty FeatureCollection', async t => {
   await convert([], devnull())
+  t.pass()
+})
+
+test('convert: file as input', async t => {
+  await convert(join(__dirname, 'fixtures', 'simple.geojson'), devnull())
   t.pass()
 })
